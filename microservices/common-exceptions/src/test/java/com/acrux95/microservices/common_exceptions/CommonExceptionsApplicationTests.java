94c0126f2a89b22e834417b048cae97179a6cd4c
Armando Cruz <acrux95.code@gmail.com>
2025-11-07 16:35:13 -0600

    Add Exception Handler

---
+++ microservices/common-exceptions/src/test/java/com/acrux95/microservices/common_exceptions/CommonExceptionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acrux95.microservices.common_exceptions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonExceptionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
